a844c233dd040cfb9c40c3609f8e1f1d9ebf14b9
ctrovato <carminetrovato@fullsail.edu>
2014-04-07 22:26:51 -0400

    Update

---
+++ Trovato_Carmine_Week1/js/script.js
@@ -0,0 +1,10 @@
+// $(function(){
+// 	$("#icons").mouseover(function(){
+// 		$(this).animate({width:"400px"});
+// });
+
+// 	$("#icons").mouseout(function(){
+// 		$(this).animate({width:"360px", height:"275px"});
+// });
+
+// 	});
